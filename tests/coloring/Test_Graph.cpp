7debb2d1f1536450fa0ebae05a92f81de791f781
Misevich Alexander WX5324211 <misevich.alexander@huawei.com>
2020-12-14 00:41:35 +0300

    Tests

--- tests/coloring/Test_Graph.cpp
+++ tests/coloring/Test_Graph.cpp
@@ -41,13 +41,65 @@ TEST(TestGraph, AddEdgesUnit)
     EXPECT_FALSE(g.has_edge(0, 4));
 }
 
-TEST(TestGraph, BruteForceColoringTwoColorUnit)
+TEST(TestGraph, ColoringListUnit)
 {
+    const size_t num_edges = 10;
     Graph g;
-    g.add_edge(0, 1);
-    g.add_edge(0, 2);
+    for (size_t i = 0; i < num_edges; ++i)
+    {
+        g.add_edge(i, i + 1);
+    }
+    EXPECT_EQ(g.get_num_vertices(), 11u);
+    EXPECT_EQ(g.brute_force_chromatic_number(), 2u);
+    EXPECT_EQ(g.lawler_chromatic_number(), 2u);
+}
 
-    [[maybe_unused]]std::size_t colors = g.brute_force_chromatic_number();
+TEST(TestGraph, ColoringTrianglesUnit)
+{
+    const size_t num_edges = 5;
+    Graph g;
+    for (size_t i = 0; i < num_edges; ++i)
+    {
+        g.add_edge(i, i + 1);
+        g.add_edge(i, i + 2);
+    }
+    EXPECT_EQ(g.get_num_vertices(), 7u);
+    EXPECT_EQ(g.brute_force_chromatic_number(), 3u);
+    EXPECT_EQ(g.lawler_chromatic_number(), 3u);
+}
+
+TEST(TestGraph, ColoringCommonCenterUnit)
+{
+    const size_t num_edges = 7;
+    Graph g;
+    for (size_t i = 1; i < num_edges; ++i)
+    {
+        g.add_edge(i, i + 1);
+        g.add_edge(i, 0);
+    }
+    // Closure
+    g.add_edge(0, 7); 
+    g.add_edge(1, 7);
+    
+    EXPECT_EQ(g.get_num_vertices(), 8u);
+    EXPECT_EQ(g.brute_force_chromatic_number(), 4u);
+    EXPECT_EQ(g.lawler_chromatic_number(), 4u);
+}
+
+TEST(TestGraph, ColoringAllIntertwinedUnit)
+{
+    const size_t num_edges = 6;
+    Graph g;
+    for (size_t i = 0; i < num_edges; ++i)
+    {
+        for (size_t j = i + 1; j < num_edges; ++j)
+        {
+            g.add_edge(i, j);
+        }
+    }
+    EXPECT_EQ(g.get_num_vertices(), 6u);
+    EXPECT_EQ(g.brute_force_chromatic_number(), num_edges);
+    EXPECT_EQ(g.lawler_chromatic_number(), num_edges);
 }
 
 }
